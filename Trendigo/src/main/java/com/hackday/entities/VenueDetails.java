38a59f1d4fd17e5b737a92c02e5ebabd80f1d02f
tarun-mehrotra <m.tarun26@gmail.com>
2016-09-10 17:15:36 +0530

    Create VenueDetails.java

---
+++ Trendigo/src/main/java/com/hackday/entities/VenueDetails.java
@@ -0,0 +1,68 @@
+package com.hackday.entities;
+
+public class VenueDetails {
+
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private double latitude;
+    private double longitude;
+    private String fullAddress;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getFullAddress() {
+        return fullAddress;
+    }
+
+    public void setFullAddress(String fullAddress) {
+        this.fullAddress = fullAddress;
+    }
+}
