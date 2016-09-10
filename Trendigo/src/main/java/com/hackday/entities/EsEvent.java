ead3d18704ff8ccba635e0a1dcbdd2c4d132cd22
tarun-mehrotra <m.tarun26@gmail.com>
2016-09-10 17:13:55 +0530

    Update EsEvent.java

--- Trendigo/src/main/java/com/hackday/entities/EsEvent.java
+++ Trendigo/src/main/java/com/hackday/entities/EsEvent.java
@@ -1,7 +1,110 @@
 package com.hackday.entities;
 
+import java.util.List;
+
 /**
  * Created by yogeesh.rajendra on 9/10/16.
  */
 public class EsEvent {
+  
+  private String eventId;
+    private String eventName;
+    private Long startTime;
+    private Long endTime;
+    private String location;
+    private VenueDetails venue;
+    private String label;
+    private String eventUrl;
+    private String bannerUrl;
+    private List<String> categories;
+    private List<String> tags;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Long startTime) {
+        this.startTime = startTime;
+    }
+
+    public Long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Long endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public VenueDetails getVenue() {
+        return venue;
+    }
+
+    public void setVenue(VenueDetails venue) {
+        this.venue = venue;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getEventUrl() {
+        return eventUrl;
+    }
+
+    public void setEventUrl(String eventUrl) {
+        this.eventUrl = eventUrl;
+    }
+
+    public String getBannerUrl() {
+        return bannerUrl;
+    }
+
+    public void setBannerUrl(String bannerUrl) {
+        this.bannerUrl = bannerUrl;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+  
 }
